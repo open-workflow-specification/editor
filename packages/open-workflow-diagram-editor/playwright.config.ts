96509d0d9154cc3fd9c90305747d49a03f69d5c4
kumaradityaraj <sedulous.0007@gmail.com>
2026-07-23 13:11:24 +0530

    Timeout for tests
    Signed-off-by: kumaradityaraj <sedulous.0007@gmail.com>

--- packages/open-workflow-diagram-editor/playwright.config.ts
+++ packages/open-workflow-diagram-editor/playwright.config.ts
@@ -19,6 +19,10 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "tests-e2e",
 
+  expect: {
+    timeout: 30000,
+  },
+
   use: {
     baseURL: "http://localhost:6006",
   },
